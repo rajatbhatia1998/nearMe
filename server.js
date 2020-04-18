492eaef9ada92fcb9cd0b28f2fc7b710c0b1d849
Rajat Bhatia <rajat.bhatia859@gmail.com>
2020-04-18 21:40:41 +0530

    make it better

--- server.js
+++ server.js
@@ -26,14 +26,13 @@ app.use('/api/room',room)
 
 
 if(process.env.NODE_ENV==='production'){
-    
-   
-
-}
-app.use(express.static('client/build'))
+    app.use(express.static('client/build'))
 app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "client", "build", "index.html"));
 });
+
+}
+
 
 
 const port  = 4000
