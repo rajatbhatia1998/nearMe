8a71cbfc1cb178ab13f8c39f7ec202dc51bc3b72
Rajat Bhatia <rajat.bhatia859@gmail.com>
2020-05-30 08:40:22 +0530

    Git final commit

--- server.js
+++ server.js
@@ -30,9 +30,9 @@ app.use('/api/room',room)
 if(process.env.NODE_ENV==='production'){
     
     app.use(express.static(path.join(__dirname, "cl", "build")));
-    console.log(path.join(__dirname, "cl", "build"))
+    console.log(path.join(__dirname, "client", "build"))
     app.get("*", (req, res) => {
-        res.sendFile(path.join(__dirname, "cl", "build", "index.html"));
+        res.sendFile(path.join(__dirname, "client", "build", "index.html"));
     });
 
 }
