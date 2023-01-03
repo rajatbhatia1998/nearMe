8e06da41f209ad4f7a7aa2c51cfb1cde2443e48f
Rajat Bhatia <rajat.bhatia859@gmail.com>
2023-01-03 17:24:09 +0530

    Update server.js

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.use('/api/room',room)
 
 if(process.env.NODE_ENV==='production'){
     
-    app.use(express.static(path.join(__dirname, "cl", "build")));
+    app.use(express.static(path.join(__dirname, "client", "build")));
     console.log(path.join(__dirname, "client", "build"))
     app.get("*", (req, res) => {
         res.sendFile(path.join(__dirname, "client", "build", "index.html"));
